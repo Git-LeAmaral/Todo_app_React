9bd5e54615a5c5c9ea453caacf7b3b40fdfdcf19
Leandro Amaral <lramaral02m@gmail.com>
2023-11-20 15:08:49 -0300

    Adicionando funcionalidades de buscas e filtros

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import Todo from './components/Todo';
 import "./App.css";
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
 
@@ -21,12 +23,16 @@ function App() {
       isCompleted: false,
     },
     {
-      id: 1,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
     }
   ]);
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -61,8 +67,18 @@ function App() {
     <div className="app">
         <h1>Lista de Tarefas</h1>
 
+        <Search search={search} setSearch={setSearch} />
+        <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+
         <div className="todo-list">
-          {todos.map(( todo ) => (
+          {todos
+            .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+            .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+            .sort((a, b) => sort === "Asc" 
+                ? a.text.localeCompare(b.text)
+                :b.text.localeCompare(a.text)
+            )
+            .map(( todo ) => (
 
             <Todo key={todo.id} 
                 todo={todo} 
